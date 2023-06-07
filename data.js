b2662bc652df2cbe061b04c272a63f05f74099d3
semopspth <chuwah21@gmail.com>
2023-06-08 07:59:26 +0800

    upload

---
+++ data.js
@@ -0,0 +1,347 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-f",
+      "name": "F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0628907707262556,
+          "pitch": 0.047431177642856426,
+          "rotation": 0,
+          "target": "5-a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.742947174055292,
+          "pitch": -0.1822197331699087,
+          "title": "第六個字的拼音",
+          "text": "shàn"
+        },
+        {
+          "yaw": 1.9931422907211847,
+          "pitch": -0.012250475686888862,
+          "title": "第七個字的拼音",
+          "text": "&nbsp;lè"
+        }
+      ]
+    },
+    {
+      "id": "1-e",
+      "name": "E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -3.112373558297598,
+        "pitch": 0.02658116082276507,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04729786137543357,
+          "pitch": 0.08095015194019695,
+          "rotation": 0,
+          "target": "4-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.633508075171994,
+          "pitch": -0.16971840027622065,
+          "title": "第五個字的拼音",
+          "text": "mǎ"
+        }
+      ]
+    },
+    {
+      "id": "2-d",
+      "name": "D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0610544129044843,
+          "pitch": 0.20228228654513636,
+          "rotation": 0,
+          "target": "4-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4260116393585882,
+          "pitch": 0.08952104817539563,
+          "title": "第四個字的拼音",
+          "text": "shèng"
+        }
+      ]
+    },
+    {
+      "id": "3-c",
+      "name": "C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.107770675507009,
+          "pitch": 0.05285838086230221,
+          "rotation": 0,
+          "target": "4-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5602091909526141,
+          "pitch": -1.5107450442855956,
+          "title": "第三個字的拼音",
+          "text": "huì"
+        }
+      ]
+    },
+    {
+      "id": "4-b",
+      "name": "B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.5129777421410235,
+        "pitch": -0.05644968125444194,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9943984394386973,
+          "pitch": 0.14360417795809965,
+          "rotation": 0,
+          "target": "2-d"
+        },
+        {
+          "yaw": 0.24805307776637164,
+          "pitch": 0.09576891239711394,
+          "rotation": 0,
+          "target": "3-c"
+        },
+        {
+          "yaw": 2.048496846369357,
+          "pitch": 0.17396069832706118,
+          "rotation": 0,
+          "target": "5-a"
+        },
+        {
+          "yaw": -1.3942075459367018,
+          "pitch": 0.037990427960206574,
+          "rotation": 0,
+          "target": "1-e"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4510955155320318,
+          "pitch": -0.2966567168844918,
+          "title": "第二個字的拼音",
+          "text": "zhǔ"
+        },
+        {
+          "yaw": 0.47777396248818427,
+          "pitch": -0.6665924476585658,
+          "title": "第八個字的拼音",
+          "text": "&nbsp;xiǎo"
+        }
+      ]
+    },
+    {
+      "id": "5-a",
+      "name": "A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.54805870616482,
+        "pitch": 0.04988122551537977,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4351334702850593,
+          "pitch": 0.03766863294191758,
+          "rotation": 0,
+          "target": "4-b"
+        },
+        {
+          "yaw": -0.38404751182056884,
+          "pitch": 0.15435271230315806,
+          "rotation": 0,
+          "target": "0-f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8770419268334155,
+          "pitch": -0.10020473005055308,
+          "title": "第一個字的拼音",
+          "text": "xiàn<br>"
+        },
+        {
+          "yaw": 0.7160603348031458,
+          "pitch": -0.8190429595114423,
+          "title": "第九個字的拼音",
+          "text": "xué"
+        }
+      ]
+    }
+  ],
+  "name": "22-23普通話遊蹤活動",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
